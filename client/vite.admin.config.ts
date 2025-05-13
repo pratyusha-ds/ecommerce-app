051dc7791dc648924ed1e499de57c4d1f07e0f71
Pratyusha <167186378+pratyusha-ds@users.noreply.github.com>
2025-05-13 21:17:04 +0200

    Admin redirection in netlify Fix-3

--- client/vite.admin.config.ts
+++ client/vite.admin.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, "index.html"),
+        admin: path.resolve(__dirname, "admin.html"),
       },
     },
-    outDir: "dist",
+    outDir: "dist-admin",
   },
 });
