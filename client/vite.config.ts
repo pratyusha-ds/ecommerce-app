2bd36f8c1ab74d9905f1aabb6c8c2e5def3c8922
Pratyusha <167186378+pratyusha-ds@users.noreply.github.com>
2025-05-13 20:19:05 +0200

    Creating admin path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        admin: path.resolve(__dirname, "admin.html"),
+      },
+      output: {
+        dir: "dist",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
