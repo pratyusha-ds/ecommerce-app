923385648861acee8784da45f63e5d7f59fc6afc
Pratyusha <167186378+pratyusha-ds@users.noreply.github.com>
2025-05-14 16:33:18 +0200

    Fix: Redirects 4

--- client/vite.admin.config.ts
+++ client/vite.admin.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  publicDir: "public",
   build: {
     outDir: "dist-admin",
     rollupOptions: {
